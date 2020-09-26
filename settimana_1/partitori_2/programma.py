d9715d10e6284d08b0552095b9a220970f88c2fa
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-09-26 15:16:27 +0200

    aggiornati grafici e risultati

--- settimana_1/partitori_2/programma.py
+++ settimana_1/partitori_2/programma.py
@@ -17,9 +17,9 @@ g.data.yData = y[1:]
 g.data.dxData = dx[1:]
 g.data.dyData = dy[1:]
 
-init = [0, 0]
+init = [1, 0]
 def f(x, a, b) :
-    return a * x + b
+    return x / a + b
 
 g.fitParams.model = f
 g.fitParams.params = init
@@ -29,10 +29,11 @@ g.plotParams.plot_errors = False
 g.plotParams.plot_model = True
 g.plotParams.title = 'partitore'
 g.plotParams.nome_asse_y = 'letture ADC'
-g.plotParams.unita_asse_y = '[u.a]'
+g.plotParams.unita_asse_y = '[V]'
 g.plotParams.nome_asse_x = 'tempo'
-g.plotParams.unita_asse_x = '[s]'
+g.plotParams.unita_asse_x = '[V]'
 g.fitParams.absolute_sigma = False
 g.plotParams.plot_residui = True
+g.plotParams.residui_normalizzati = False
 g.fit()
 g.plot()
