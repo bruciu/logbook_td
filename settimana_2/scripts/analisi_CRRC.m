88913764593f6dacc16c1927b853257a1fe6b473
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-10-08 14:57:29 +0200

    /

--- settimana_2/scripts/analisi_CRRC.m
+++ settimana_2/scripts/analisi_CRRC.m
@@ -1,4 +1,5 @@
 [freqs, As, phis] = leg_wavf3pars("../dati_seconda_settimana/passabanda/acquispassa2.txt");
+[freqs, As, phis] = leg_wavf3pars("../../../../esp_2/prova_estremis_crrc/crrc_3.txt");
 
 % m = 0.0000313944;
 % dm = 0.0000000185;
@@ -19,14 +20,20 @@ A_crrc_raw;
 A_crrc = @(C1, C2, R1, R2, f) A_crrc_raw(C1, C2, f, 1/(2.*pi.*C1.*R1), 1/(2*pi.*C2.*R2));
 A_crrc_mod = @(C1, C2, R1, R2, Rout, f) abs(H_crrc(C1, C2, R1, R2, f) .* Rout ./...
     (Rout + Zth_crrc(C1, C2, R1, R2, f)));
+A_crrc_mod2 = @(C1, C2, R1, R2, Rout, Cout, f) A_crrc_mod(C1, C2, R1, R2, 1 ./ (1 / Rout + 1i .* 2.*pi .* f .* Cout), f);
 %A_crrc(C1, C2, R1, R2, f) * Rout /...
  %   (Rout + Rth_crrc(C1, C2, R1, R2, f));
 
-f = @(C1, C2, R1, R2, Rout, x) A_crrc_mod(C1, C2, R1, R2, Rout, x);
+f = @(C1, C2, R1, R2, Rout, x) A_crrc_mod(C1 * 1e-9, C2 * 1e-9, R1 * 1e3, R2 * 1e3, Rout * 1e3, x);
+f2 = @(C1, C2, R1, R2, Rout, Cout, x) A_crrc_mod2(C1 * 1e-9, C2 * 1e-9, R1 * 1e3, R2 * 1e3, Rout * 1e3, Cout * 1e-9, x);
 fitfun = fittype(f);
-X0 = [C1_val, C2_val, R1_val, R2_val, 100e3];
+fitfun2 = fittype(f2);
+X0 = [C1_val / 1e-9, C2_val / 1e-9, R1_val / 1e3, R2_val / 1e3, 50e3 / 1e3];
+X02 = [C1_val / 1e-9, C2_val / 1e-9, R1_val / 1e3, R2_val / 1e3, 50e3 / 1e3, 0.01];
 [fitted_curve, gof] = fit(freqs, As, fitfun, "StartPoint", X0);
+[fitted_curve2, gof2] = fit(freqs, As, fitfun2, "StartPoint", X02);
 coeffvals = coeffvalues(fitted_curve);
+coeffvals2 = coeffvalues(fitted_curve2);
 errors = confint(fitted_curve);
 for x = coeffvals
     x
@@ -37,7 +44,10 @@ hold on;
 plot(freqs, A_crrc(C1_val, C2_val, R1_val, R2_val, freqs))
 plot(freqs, A_crrc_mod(C1_val, C2_val, R1_val, R2_val, 50e3, freqs))
 c = coeffvals;
-plot(freqs, A_crrc_mod(c(1), c(2), c(3), c(4), c(5), freqs))
+c2 = coeffvals2;
+plot(freqs, f(c(1), c(2), c(3), c(4), c(5), freqs))
+plot(freqs, f2(c2(1), c2(2), c2(3), c2(4), c2(5), c2(6), freqs))
+c2
 
 % phis = phis - m * freqs;
 % plot(freqs, phis)
