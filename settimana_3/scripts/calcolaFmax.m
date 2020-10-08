16e47daac99393c373f39b350d48a39c07d30262
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-10-08 22:13:57 +0200

    /

--- settimana_3/scripts/calcolaFmax.m
+++ settimana_3/scripts/calcolaFmax.m
@@ -1,49 +1,93 @@
-function [f, df] = calcolaFmax(yy, dt, multiplier)
+function [f, df] = calcolaFmax(yy, dt, multiplier, plot_analsys)
+
+if (nargin < 3)
+    multiplier = 16;
+end
+
+if (nargin < 4)
+    plot_analsys = true;
+end
+
+M = multiplier;
+y_s = size(yy);
+dp = 100;
+
+% rendi yy un vettore riga
+if (y_s(1) == 1)
+    if (numel(y_s) <= 1)
+        throw MException('yy deve essere un vetore o una matrice')
+    end
+else
+    yy = yy';% trasponi
+end
 
 % numero di punti
 N = numel(yy);
 
-y_ext = 
+% estendi le yy per eseguire lo zero-padding
+yy_ext = [yy, zeros(1, (M-1)*N)];
 
+% trasformata discreta dui punti con lo zero_padding, divisa per N come se
+% fosse la trasformata normale, così è equivalente ad un semplice "infittimento"
+% della normale DFT
+FT_ext = fft(yy_ext) / (N);
 
+% considera il modulo quadro (solo prima metà)
+FT2_ext = abs(FT_ext(1:(N * multiplier / 2))).^2;
+
+% trova il massimo
+[~, k_max] = max(FT2_ext);
+
+% ora prendi tre punti lì intorno:
+x1 = k_max - 1;
+y1 = FT2_ext(k_max - 1);
+x2 = k_max;
+y2 = FT2_ext(k_max);
+x3 = k_max + 1;
+y3 = FT2_ext(k_max + 1);
+
+if (plot_analsys)
+    figure;
+    interval = max(1, k_max - dp):min(N*M, k_max + dp);
+    plot(interval - 1, FT2_ext(interval), '-+');
+    hold on;
+    
+    interval = max(1, k_max - 1):min(N*M, k_max + 1);
+    plot(interval - 1, FT2_ext(interval), 'dr');
 end
 
-
-function [f, df] = calcolaFmax_old(yy, dt, multiplier)
-%CALCOLAFMAX Summary of this function goes here
-%   Detailed explanation goes here
-N = numel(yy);
-y_orig = yy;
-yy = [yy zeros(1, (multiplier-1)*numel(yy))];
-FT = fft(yy) / (N);
-FT2 = abs(FT(1:(N * multiplier / 2))).^2;
-plot((1:1:(100 * multiplier)) / multiplier, FT2(1:100 * multiplier), '-d')
-[ymax, kmax] = max(FT2);
-x1 = kmax -1;
-y1 = FT2(kmax - 1);
-x2 = kmax + 1;
-y2 = FT2(kmax + 1);
-A = [1, x1, x1^2;
+% trova la parabola passente per questi tre punti:
+A = [
+    1, x1, x1^2;
     1, x2, x2^2;
-    1, kmax, kmax^2];
-a = A\[y1; y2; ymax];
+    1, x3, x3^2
+];
+a = A\[y1; y2; y3]; % coefficienti della parabola a1 + a2*x + a3*x^2
 
-hold on
-plot([x1, x2, kmax] / multiplier, [y1, y2, ymax], 'd')
+DELTA = (N - 1)./(sum(abs(fft(yy)/N).^2) - 2 .* FT2_ext(k_max));
 
-tmp = (x1-5):0.001:(x2 + 5);
-plot(tmp / multiplier, a(1) + a(2)*tmp + a(3) * tmp.^2)
-plot([- a(2)/(2*a(3)), - a(2)/(2*a(3))] / multiplier, [0, 1])
+% migliora la stima di k_max
+k_max = - a(2)/(2*a(3));
 
-DELTA = (N - 1)./(sum(abs(fft(y_orig) / N).^2) - 2 .* FT2(kmax));
-kmax = - a(2)/(2*a(3));
-f = (kmax - 1)/(dt * N * multiplier);
-%t = sqrt(-1/(DELTA*a(3)));
+% trova la frequeza associata
+f = (k_max - 1)/(dt * N * multiplier);
+
+% trova l'incertezza (in unità di indici)
 t = sqrt(-1/(DELTA*a(3)));
-DELTA
+
+% calcola l'incertezza in frequenza
 df = t/(dt * N * multiplier);
 
-hold off
+if (plot_analsys)
+    xx = x1:0.01:x3;
+    p = @(x) a(1) + a(2).*x + a(3).*x.^2;
+    plot(xx - 1, p(xx), 'r');
+    plot([k_max, k_max] - 1, [p(k_max) * 0.98, p(k_max) * 1.02], 'r');
+    hold off;
+end
 
 end
+
+
+
 
--- settimana_3/scripts/trova_frequenza_dati.m
+++ settimana_3/scripts/trova_frequenza_dati.m
@@ -1,9 +1,9 @@
-[tt, yy] = leg_wavf("data_sinusoidale/acquisizione1.txt");
+[tt, yy] = leg_wavf("../data_sinusoidale/acquisizione1.txt");
 
 plot_N = 1000;
 
 plot(tt(1:plot_N), yy(1:plot_N), '+')
 
-[f, df] = calcolaFmax(yy', tt(2) - tt(1), 50);
+[f, df] = calcolaFmax(yy, tt(2) - tt(1));
 fprintf("%f +- %f\n", f, df)
 
