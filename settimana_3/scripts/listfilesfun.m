1d14bd585afb8e8abbbc6c492936efb552a15b49
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-10-09 17:54:46 +0200

    Update listfilesfun.m

--- settimana_3/scripts/listfilesfun.m
+++ settimana_3/scripts/listfilesfun.m
@@ -12,7 +12,8 @@ for i = 3:numel(lista)
     name = strcat(strcat(Name_folder, '/') , lista(i).name);
     %display(name);
     Titolo1 = strrep(lista(i).name,'.txt','');
-    Titolo2 = strcat(Titolo1, ' completo')
+    Titolo2 = strcat(Titolo1, ' completo');
+    Titolo3 = 'FFT';
 %     Titolo = name(1:numel(name)-4);
 %     display(Titolo);
     %Titolo = strsplit(num2str(1:(numel(Name_folder)-4)));
@@ -27,8 +28,8 @@ for i = 3:numel(lista)
     [fmax, dfmax] = calcolaFmax(yy, ((xx(3) - xx(2))/2));
     hold on
     grid on
-    plot(f, A, 'black.', 'LineWidth', 0.5)
-    plot(f, A, 'yellow-', 'LineWidth', 0.5)
+    stem(f, A, 'black.', 'LineWidth', 0.5)
+    %plot(f, A, 'yellow-', 'LineWidth', 0.5)
     minx = max(0, fmax - 10^(3));
     maxx = fmax+10^(3);
     axis([minx  maxx  min(A)-0.1 max(A)+0.1])
@@ -41,13 +42,32 @@ for i = 3:numel(lista)
     figure;
     hold on
     grid on
-    plot(f, A, 'black.', 'LineWidth', 0.5)
-    plot(f, A, 'yellow-', 'LineWidth', 0.5)
+    stem(f, A, 'black.', 'LineWidth', 0.5)
+    %plot(f, A, 'yellow-', 'LineWidth', 0.5)
     axis([0 max(f) min(A)-0.1 max(A)+0.1])
+    xlabel("Frequenza [Hz]")
+    ylabel('Ampiezza FFT')
+    title(Titolo2)
     hold off;
     %plot(vector(:,1), vector(:,2), 'green.-', 'LineWidth', 0.5)%, '.-')%, 'LineWidth', 2, '.-')
     %axis([min(vector(:,1)) max(vector(:,1)) min(vector(:,2)) max(vector(:,2))]);
 %, '.-');
+    figure;
+    hold on
+    grid on
+    z = complex(A.*cos(phi), A.*sin(phi));
+    compass(z);
+    %stem(f, A, 'black.', 'LineWidth', 0.5)
+    %plot(f, A, 'yellow-', 'LineWidth', 0.5)
+    minx = max(0, fmax - 10^(3));
+    maxx = fmax+10^(3);
+    %axis([minx  maxx  min(A)-0.1 max(A)+0.1])
+    %xlabel('-\pi \leq \itt \leq \pi')
+    %xlabel(strcat(strcat(sprintf('%d', minx), 'Hz \leq  frequenza  \leq '),sprintf('%d Hz',maxx)))
+    xlabel('Frequenza [Hz]')
+    ylabel('Ampiezza FFT')
+    title(Titolo1)
+    hold off;
     figure;
 end
 %vector = zeros(1, 3);
