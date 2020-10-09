982101b89df7729292cc395f7e0f57c6525c2959
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-10-09 17:54:06 +0200

    /

--- settimana_3/scripts/esercizio_3.m
+++ settimana_3/scripts/esercizio_3.m
@@ -1,9 +1,9 @@
 
 % tempo totale
-T = 5; % secondi
+T = 10; % secondi
 
 % frequenza del segnale
-f_s = 15; % Hz
+f_s = 1; % Hz
 
 % lunghezza del vettore (numero di punti)
 n = 14;
@@ -16,24 +16,42 @@ t = linspace(0, T, L) + randn(1, L) * T/(L)*0;
 y = cos(f_s * 2 * pi * t) + randn(1, L) * 0;
 
 [freqs, Ampiezze, fase] = myFFT(y, T/L) ;
+[A_max, i_max] = max(Ampiezze);
 
 subplot(2, 1, 1);
-plot(t, y, '+')
+plot(t, y, '-')
+title(sprintf("L = %d  freq. sig. = %.2f, T_{tot} = %f", L, f_s, T));
+xlabel("tempo [s]");
+ylabel("valore [u]");
 
 subplot(2, 1, 2);
-%Delta_f = 
-interessanti = [1:100];
-plot(freqs(interessanti), Ampiezze(interessanti), '-d')
-%plot(freqs(interessanti), fase(interessanti), '-d');
+f_max = freqs(i_max);
+interessanti = [max(0, floor(f_max - max(10, f_max / 4))), min(L, floor(f_max + max(10, f_max / 4)))];
+stem(freqs, Ampiezze, 'dr')
+xlim(interessanti);
 %set(gca, "YScale", 'log');
 
 fprintf("massima ampiezza = %f", max(Ampiezze));
 
 
+% ZERO PADDING
+M = 100;
+[freqs, Ampiezze, fase] = myFFT([y, zeros(1, numel(y) * (M - 1))], T/L);
+hold on
+stem(freqs, Ampiezze * M, '.b')
+hold off
+
+legend("abs(FFT)", sprintf("abs(FFT) + zero-padding (M = %d)", M))
+title("spettro");
+xlabel("frequenze [Hz]");
+ylabel("ampiezza [u]");
+grid();
+
 numel(y)
 [f, df] = calcolaFmax(y, T/L);
 fprintf("%f +- %f\n", f, df)
 
+saveas(gcf,'tmp/prova.png')
 
 
 
