a0f40d7cedfcdae8818e4d5c1243f1d178f5a3e2
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-10-15 23:58:00 +0200

    script

---
+++ settimana_4/scripts/Nucleo.m
@@ -0,0 +1,24 @@
+classdef Nucleo
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        sp = 0 %porta seriale - quando non è un numero, allora è aperta la comunicazione
+    end
+    
+    methods
+        function obj = apri_comunicazione(obj, porta)
+            if obj.isOpen()
+                obj.close();
+            end
+            obj.sp = serialport(porta, 2e6);
+        end
+        function [aperto] = isOpen(obj)
+            aperto = ~isnumeric(obj.sp);
+        end
+        function obj = close(obj)
+            obj.sp = 0;
+        end
+    end
+end
+
---
+++ settimana_4/scripts/prova.m
@@ -0,0 +1,4 @@
+
+n = Nucleo;
+
+n.ucle
