a1e04f3c742ab945d6405206f10d74dfc96afad9
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-10-16 17:42:47 +0200

    Create per_nucleo.m

---
+++ settimana_4/scripts/per_nucleo.m
@@ -0,0 +1,41 @@
+classdef per_nucleo < handle
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        sp = 0 %porta seriale - quando non è un numero, allora è aperta la comunicazione
+    end
+    
+    methods
+        function apri_comunicazione(obj, porta)
+            if obj.isOpen()
+                obj.close();
+            end
+            obj.sp = serialport(porta, 2e6);
+        end
+        function [aperto] = isOpen(obj)
+            aperto = ~isnumeric(obj.sp);
+        end
+        function close(obj)
+            obj.sp = 0;
+        end
+        function linea = readline(obj)
+            if ~obj.isOpen()
+                error("la comunicazione non è aperta");
+            end
+            linea = obj.sp.readline();
+        end
+        %%%NUOVE
+        function idn(obj)
+            %*IDN?
+        end
+        function prescaler(obj, value)
+            %PRESCALER{value}
+        end
+        function isPrescaler(obj)
+            %PRESCALER?
+        end
+    end
+end
+
+
