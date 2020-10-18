17c65eba3ee747528822c301662ae733cfffc0ee
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-10-18 21:58:54 +0200

    correzioni

--- settimana_4/scripts/AcquisDigit.m
+++ settimana_4/scripts/AcquisDigit.m
@@ -3,13 +3,13 @@ clear all;
 f = 1000;
 
 N_onda = 70;
-N_samples = 600;
+N_samples = 60;
 
 ftrig = N_onda * f;
 
 % funzione da generare (deve essere definita con periododo 1)
 %funz = @(x) gauspuls((x-0.5)*2*pi, 10, 0.05) * 1000 + 2050;
-funz = @(x) sin(x*2*pi) * 1000 + 2050;
+funz = @(x) sin(x*2*pi) * 10 + 2050;
 
 PS = 120e6/(f*N_onda);
 PS = round(PS);
@@ -27,6 +27,7 @@ mini.setNSamples(N_samples);
 
 mini.setPrescaler(PS);
 
+for i = 1:20
 [t, y0, y1]= mini.DACADC();
 
 %[t, y0, y1] = mini.getValues();
@@ -36,7 +37,8 @@ plot(t, y0, 'r.-');
 hold on; 
 
 plot(t, y1, 'b.-');
-
+hold off;
+end
 % mini.setDAC(false);
 % 
 % mini.setADC(false);
