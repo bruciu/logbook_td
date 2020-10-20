4941377665e8fa40ecf981ddf7ef11b198718bc1
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-10-20 21:22:06 +0200

    Update es_9.m

--- settimana_4/scripts/es_9.m
+++ settimana_4/scripts/es_9.m
@@ -2,7 +2,7 @@ clear all;
 
 N_onda = 100;
 N_samples = 600;
-N = 5;
+N = 100;
 PSrange = [120, 120e2];
 PSrange = linspace(PSrange(1), PSrange(2), N);
 ftrig = 120e6 ./ PSrange; 
@@ -17,6 +17,8 @@ mini.setNSkip(10);
 mini.setNSamples(N_samples);
 fasi = zeros(1,N);
 dfasi = zeros(1,N);
+ampli = zeros(1,N);
+dampli = zeros(1,N);
 
 for ii = 1:N
     mini.setPrescaler(PSrange(ii));
@@ -32,8 +34,11 @@ for ii = 1:N
     [freqs0, As0, dAs0, phis0, dphis0] = calcolaFFT_errori(y0 - mean(y0), 1, 1 ./ ftrig(ii));
     [freqs1, As1, dAs1, phis1, dphis1] = calcolaFFT_errori(y1 - mean(y1), 1, 1 ./ ftrig(ii));
     [maxA0, jmax] = max(As0);
+    [maxA1, ~] = max(As1);
     fasi(ii) = (mod((phis1(jmax)- phis0(jmax))*N_samples, 2*pi))/(2*pi);
     dfasi(ii) = sqrt((dphis1(jmax))^2 + (dphis0(jmax))^2)*N_samples/(2*pi);
+    ampli(ii) = (maxA1- maxA0)/maxA0;
+    dampli(ii) = sqrt( (dAs0(ii)*(2*maxA0 - maxA1)/maxA0^2)^2 + (dAs1(ii)/maxA0^2)^2 );
 end
 
 errorbar(PSrange, fasi, dfasi, 'r+');
@@ -41,5 +46,9 @@ grid()
 xlabel("Prescaler")
 ylabel("\Delta \phi [%]")
 saveas(gcf,'tmp/graph.png');
-
+figure;
+errorbar(PSrange, ampli, dampli, 'b+');
+grid()
+xlabel("Prescaler")
+ylabel("---")
 
