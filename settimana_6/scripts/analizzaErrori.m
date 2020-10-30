8e104b7483c1b08c258344c7fdd8f7e7056b7dd7
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-10-30 20:54:12 +0100

    /

--- settimana_6/scripts/analizzaErrori.m
+++ settimana_6/scripts/analizzaErrori.m
@@ -7,3 +7,13 @@ mini.calibration();
 c = CorrettoreADC;
 c.run(mini);
 c.salva("dati_calibrazione/luca_30MHz_12.5.mat");
+
+%  30Mhz 2.5
+%  30MHz 6.5
+%  30MHz 12.5
+%  60Mhz 2.5
+%  60Mhz 6.5
+%  60Mhz 12.5
+%  120Mhz 2.5
+%  120Mhz 6.5
+%  120Mhz 12.5
