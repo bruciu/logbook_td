8954ece1ae37d72ad53411f44a5956ff00e6226c
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-11-02 22:52:57 +0100

    Update es_9.m

--- settimana_6/scripts/es_9.m
+++ settimana_6/scripts/es_9.m
@@ -1,4 +1,10 @@
 mini = Nucleo;
 mini.apri_comunicazione('COM3');
-mini.writeline('I2CWRITE 63,0');
-mini.readline()
+ mini.writeline('I2CWRITE 12,0');
+% for ii = 117:128
+%     mini.writeline(sprintf('I2CWRITE %f,8', ii-1))
+%     pause(1);
+     mini.readline()
+%     pause(1);
+%     ii
+% end
