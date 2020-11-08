2a3fd132a1c4c92b21de16c1374f93d97c8ca91d
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-11-08 18:48:38 +0100

    Update es_10.m

--- settimana_7/scripts/es_10.m
+++ settimana_7/scripts/es_10.m
@@ -59,14 +59,40 @@ errorbar(f, As, dAs, '.k');
 set(gca, "XScale", 'log')
 set(gca, "YScale", 'log')
 grid();
+figure;
 
+guad = As(2:end);
+dguad = dAs(2:end);
+freqs = f(2:end);
 
+R2_val = 10e3 /2;
+R1_val = 1e3;
+C1_val = 10e-9;
+C2_val = 68e-9;
 
-
-
-
-
-
+funz = @(fti, ftd, x)1./sqrt((1 + (x./fti).^2).*(1 + (ftd./x).^2));
+fitfun = fittype(funz);
+X0 = [1/(2*pi*R1_val*C1_val), 1/(2*pi*R2_val*C2_val)];%
+[fitted_curve, gof] = fit(freqs', guad', fitfun, "StartPoint", X0, 'Weight', 1./(dguad'));
+coeffvals2 = coeffvalues(fitted_curve);
+errors = confint(fitted_curve);
+fprintf("fti = %.10f +- %.10f\n", coeffvals2(1), (errors(2, 1) - errors(1, 1))/2);
+fprintf("ftd = %.10f +- %.10f\n", coeffvals2(2), (errors(2, 2) - errors(1, 2))/2);
+hold on;
+errorbar(freqs, guad, dguad, 'k.');
+set(gca, 'XScale', 'log')
+set(gca, 'YScale', 'log')
+plot(freqs, funz(coeffvals2(1), coeffvals2(2), freqs), 'r');
+xlabel("Frequenza [Hz]")
+ylabel("Guadagno")
+legend('dati', 'fit')
+set(gca, 'XScale', 'log')
+set(gca, 'YScale', 'log')
+grid()
+%plot(f, funz(X0(1), X0(2), X0(3), X0(4), X0(5), X0(6), f), 'b');
+hold off
+saveas(gcf,'tmp/provaes10.png');
+hold off;
 
 
 
