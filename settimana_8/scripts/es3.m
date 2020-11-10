b80795e55e0bd39ab153ebc3f83eb2b33073c6c9
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-11-10 17:50:36 +0100

    g

--- settimana_8/scripts/es3.m
+++ settimana_8/scripts/es3.m
@@ -3,14 +3,15 @@ R2 = 100;
 GBP = 2.8e6;
 w = linspace(10^4, 10^8, 1000);
 tau = 1/GBP
-Guad = @(x) GuadagnoG(beta(R1, R2), 1./(1 + 1i.*x.*tau));
+A0 = 10^(110/20);
+Guad = @(x) GuadagnoG(beta(R1, R2), A0./(1 + 1i.*x.*tau));
 y = abs(Guad(w));
 plot(w, y)
-%ylim([min(y), max(y)]);
+ylim([min(y), max(y)]);
 xlabel("$\omega$ [$rad^{-1}$]", 'Interpreter', 'Latex')
 ylabel("Loop Gain G")
 legend('$G(\omega) = \frac{A(\omega)}{1 + A(\omega) \beta}$', 'Interpreter', 'Latex', 'Fontsize', 12)
 set(gca, 'XScale', 'log')
-set(gca, 'YScale', 'log')
+%set(gca, 'YScale', 'log')
 grid()
 saveas(gcf,'tmp/provaes3.png');
