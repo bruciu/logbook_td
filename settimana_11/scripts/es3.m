06a2a0b760c7ceb7de00d47845299243c30b0007
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-01 18:04:01 +0100

    Update es3.m

--- settimana_11/scripts/es3.m
+++ settimana_11/scripts/es3.m
@@ -7,9 +7,9 @@ mini.apri_comunicazione('COM3');
 
 
 mini.writeline(sprintf('I2CREAD? 93,%d,1', 0x0F))
-l = mini.readline();
+l = mini.readline()
 %if (l(1) ~= 'E')
 %indirizzi = [inidirizzi, i];
 
 
-fprintf("indirizzi trovati: "); disp(indirizzi);
+%fprintf("indirizzi trovati: "); disp(indirizzi);
