6644b5df609ee4d4038ad7f0c97e3173cc2c48e7
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-01 18:03:13 +0100

    h

--- settimana_11/scripts/es3.m
+++ settimana_11/scripts/es3.m
@@ -5,12 +5,11 @@ inidirizzi = [];
 mini = Nucleo;
 mini.apri_comunicazione('COM3');
 
-for i = 0:127
-     mini.writeline(sprintf('I2CWRITE %d,0', i))
-     l = mini.readline();
-     if (l(1) ~= 'E')
-         indirizzi = [inidirizzi, i];
-     end
-end
+
+mini.writeline(sprintf('I2CREAD? 93,%d,1', 0x0F))
+l = mini.readline();
+%if (l(1) ~= 'E')
+%indirizzi = [inidirizzi, i];
+
 
 fprintf("indirizzi trovati: "); disp(indirizzi);
