ddc857f1452996bc2b5048fe2d4b16d85f0767bc
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-12-01 17:55:11 +0100

    Update es_2_2.m

--- settimana_11/scripts/es_2_2.m
+++ settimana_11/scripts/es_2_2.m
@@ -6,7 +6,7 @@ mini = Nucleo;
 mini.apri_comunicazione('COM3');
 
 for i = 0:127
-     mini.writeline(sprintf('I2CWRITE %f,0', i))
+     mini.writeline(sprintf('I2CWRITE %d,0', i))
      l = mini.readline();
      if (l(1) ~= 'E')
          indirizzi = [inidirizzi, i];
