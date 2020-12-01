43b58f297ec1e9ce7e106339ee14f56249f82d3e
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-01 17:58:34 +0100

    Update es3.m

--- settimana_11/scripts/es3.m
+++ settimana_11/scripts/es3.m
@@ -1,17 +1,16 @@
+% indirizzi = vettore dei SAD trovati
+inidirizzi = [];
+
+% crea l'oggetto mini
 mini = Nucleo;
 mini.apri_comunicazione('COM3');
-%mini.writeline('I2CWRITE 63,8');
- for ii = 0:127
-     mini.writeline(sprintf('I2CWRITE %f,0', ii))
-     %pause(1);
+
+for i = 0:127
+     mini.writeline(sprintf('I2CWRITE %d,0', i))
      l = mini.readline();
-     mini.writeline("I2CREAD?");
-     l2 = mini.readline();
      if (l(1) ~= 'E')
-         ii
-         disp(l)
-         %disp(l2)
+         indirizzi = [inidirizzi, i];
      end
-     %pause(1);
-     %ii
- end
+end
+
+fprintf("indirizzi trovati: "); disp(indirizzi);
