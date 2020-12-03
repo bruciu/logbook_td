0883552618f58a09cc9e617d9e2aedfbce84e446
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-03 16:17:21 +0100

    Update provaSerena.m

--- settimana_11/scripts/provaSerena.m
+++ settimana_11/scripts/provaSerena.m
@@ -4,7 +4,7 @@ SAD = 93;
 dev = I2Cdevice('COM3',SAD);
 
 CTRL_REG1 = 0x20;
-CTRL_REG1_val = 0b10000000; % acensionie in modalità single shot
+CTRL_REG1_val = 0b11100000; % acensionie in modalità single shot
 dev.write(CTRL_REG1,[CTRL_REG1_val]);
 
 SUB = 0x2B + 0x80;
@@ -16,6 +16,15 @@ end
 
 fprintf('Temperatura: %fC\n',42.5+TT/480);
 
+SUB = 0x28 + 0x80;
+bytes = dev.read(SUB, 3);
+PP = bytes(3)*2^16 + bytes(2)*256 + bytes(1);
+if PP>2^23
+    PP = PP-2^24;
+end
+P = PP / 4096;
+
+fprintf('Pressione: %fmbar\n',P);
 % 1 = 0x1 = 0b1;
 % 0x43 = 67
 % 0b10 = 2
