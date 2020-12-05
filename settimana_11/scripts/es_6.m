67367641a50a32578da1034290599226636b5c6c
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-05 10:29:09 +0100

    j

--- settimana_11/scripts/es_6.m
+++ settimana_11/scripts/es_6.m
@@ -6,16 +6,19 @@ barometro = Barometro("COM3");
 CTRL_REG1 = 0x20;
 CTRL_REG1_val = 0b11100000; % acensionie in modalità 12.5Hz
 barometro.write(CTRL_REG1,[CTRL_REG1_val]);
+dec2bin(barometro.read(CTRL_REG1,1))
 
 RES_CONF = 0x10;
 RES_CONF_val = 0b0;
+%RES_CONF_val = 0x71;
 barometro.write(RES_CONF,[RES_CONF_val]);
+dec2bin(barometro.read(RES_CONF,1))
 
 tempi = [];
 pressioni = [];
 temperature = [];
 
-for i = 1:1000
+for i = 1:200
     disp(i);
     [p, t] = barometro.readValue();
     tempi = [tempi, now];
