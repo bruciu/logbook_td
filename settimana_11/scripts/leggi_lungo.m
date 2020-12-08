3c950a2c955566ea9f610ff2855a98f60ef901db
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-12-08 18:53:13 +0100

    Update leggi_lungo.m

--- settimana_11/scripts/leggi_lungo.m
+++ settimana_11/scripts/leggi_lungo.m
@@ -2,13 +2,45 @@
 
 % E:\
 
-filename = 'data/datasett11_lunga_corretto.txt';
+filename = 'data/datasett11_serena_corretto.txt';
 delimiterIn = '\t';
 A = importdata(filename,delimiterIn);
+p = A(:, 2);
+t = A(:, 1)/60000;
+T = A(:, 3);
 
+figure;
 hold on;
-plot(A(:,2));
-sm = smussaf(A(:,2), 0.01);
-plot(sm);
+plot(t, p, '.k');
+sm = smussaf(p, 0.05);
+plot(t, sm, "linewidth", 2);
+hold off;
+xlabel("tempo [min]");
+ylabel("pressione [mbar]")
+grid()
+legend("letture", "smooth 5% armoniche", "location", "best");
+xlim([min(t), max(t)]);
+
+figure;
+hold on;
+plot(t, T, '.k');
+sm = smussaf(T, 0.05);
+plot(t, sm, "linewidth", 2);
+hold off;
+xlabel("tempo [min]");
+ylabel("temperatura [°C]")
+grid()
+legend("letture", "smooth 5% armoniche", "location", "best");
+xlim([min(t), max(t)]);
+
+figure;
+[freqs, A] = myFFT(T - mean(T), (max(p)-min(p)) / length(p));
+plot(A.^2);
+set(gca, "XScale", 'log');
+set(gca, "YScale", 'log');
+hold on;
+fplot(@(x) 0.1/x.^2 + 1e-7, xlim);
+fplot(@(x) 0.1/x + 1e-7, xlim);
+
 hold off;
 
