6f3bf18e1db954c7777a251cc7f39e09d22fee3b
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-12-10 18:54:20 +0100

    Update es_5_solo_fft.m

--- settimana_12/scripts/es_5_solo_fft.m
+++ settimana_12/scripts/es_5_solo_fft.m
@@ -1,3 +1,10 @@
 
 
-[] = myFFT(uu, );
+[freqs, As] = myFFT(uu, (max(tempi) - min(tempi))/length(tempi));
+
+plot(freqs, As.^2);
+set(gca, "XScale", "log")
+set(gca, "YScale", "log")
+grid();
+xlabel("frequenze [Hz]");
+ylabel("ampiezze quadre");
