fd386785d79ef0f1fc8afe7d0a7fd7bd502c5bc0
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-10 18:39:08 +0100

    Create es5.m

---
+++ settimana_12/scripts/es5.m
@@ -0,0 +1,28 @@
+clear all
+
+ig = Igrometro('COM3');
+
+N = 10;
+uu = [];
+tt = [];
+
+for i = 1:N
+    [U, T] = ig.readValue();
+    uu = [uu, U];
+    tt = [tt, T];
+    yyaxis left;
+    plot(tt);
+    yyaxis right;
+    plot(uu);
+end
+
+xlabel("numero di lettura");
+yyaxis left;
+ylabel("temperatura [°C]");
+yyaxis right;
+ylabel("umidità [PS*100]");
+grid();
+meanT = mean(tt);
+sdvT = sqrt(var(tt));
+meanU = mean(uu);
+sdvU = sqrt(var(uu));
