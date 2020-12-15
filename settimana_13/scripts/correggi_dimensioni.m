c25560828b8a7302b8ac68f157751321821a66cf
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-12-15 17:25:42 +0100

    Update correggi_dimensioni.m

--- settimana_13/scripts/correggi_dimensioni.m
+++ settimana_13/scripts/correggi_dimensioni.m
@@ -1,7 +1,26 @@
-function [outputArg1,outputArg2] = correggi_dimensioni(inputArg1,inputArg2)
-%CORREGGI_DIMENSIONI Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function correggi_dimensioni()
+
+xlim_tmp = xlim;
+ylim_tmp = ylim;
+zlim_tmp = zlim;
+
+mean_x = mean(xlim_tmp);
+range_x = xlim_tmp(2) - xlim_tmp(1);
+
+mean_y = mean(ylim_tmp);
+range_y = ylim_tmp(2) - ylim_tmp(1);
+
+mean_z = mean(zlim_tmp);
+range_z = zlim_tmp(2) - zlim_tmp(1);
+
+range = max([range_x, range_y, range_z]);
+range = range / 2;
+
+xlim([mean_x - range, mean_x + range]);
+ylim([mean_y - range, mean_y + range]);
+zlim([mean_z - range, mean_z + range]);
+
+set(gcf,'Position',[100 100 800 800])
+
 end
 
