4022389c30d0ce47963fda9b8b7b090a486eb5ac
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-15 21:12:24 +0100

    d

--- settimana_13/scripts/es_4_2.m
+++ settimana_13/scripts/es_4_2.m
@@ -3,7 +3,7 @@ dev = I2Cdevice("com3", 0x68);
 
 %impostazioni iniziali:
 PWR_MGMT_1 = 0x6B;
-PWR_MGMT_1_val = 0b00000000;
+PWR_MGMT_1_val = 0b00010000;
 dev.write(PWR_MGMT_1, PWR_MGMT_1_val);
 
 SMPRT_DIV = 0x19;
@@ -12,4 +12,4 @@ dev.write(SMPRT_DIV, SMPRT_DIV_val);
 %   scegliamo di dividere il sample rate di
 %   80 (in scala decimale), così di avere una frequneza di campionamento di 100 Hz
 
-[ax, ay, az, omx, omy, omz, T] = leggivalues(0b00010000 ,0b00011000, dev);
+[ax, ay, az, omx, omy, omz, T] = leggivalues(0b00010000 ,0b00011000, dev, 8192/2, 16.4)
--- settimana_13/scripts/leggivalues.m
+++ settimana_13/scripts/leggivalues.m
@@ -1,4 +1,4 @@
-function [ax, ay, az, omx, omy, omz, T] = leggivalues(ACCEL_CONFIG_val, GYRO_CONFIG_val, dev)
+function [ax, ay, az, omx, omy, omz, T] = leggivalues(ACCEL_CONFIG_val, GYRO_CONFIG_val, dev, conva, convg)
 %LEGGIVALUES Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -15,17 +15,43 @@ ACCELHL = dev.read(0x3B, 6);
 GYROHL = dev.read(0x43, 6);
 TEMPHL = dev.read(0x41, 2);
 
-%concatenazione a vettore
-ACCEL = 
+%concatenazione a vettore e complemento a 2:
+ACCEL = converti_vettore(ACCELHL);
+GYRO = converti_vettore(GYROHL);
+TEMP = unisci_bytes(TEMPHL(1), TEMPHL(2));
+TEMP = comp2(TEMP);
 
-ax = ACCEL(1:2)
-ay = ACCEL(3:4)
-az = ACCEL(5:6)
-omx = GYRO(1:2)
-omy = GYRO(3:4)
-omz = GYRO(5:6)
-T = TEMP
-%concatenazione complemento a 2:
+%conversione accelerazione a m/s^2:
+acc = ACCEL./conva; %a mano
+acc = acc .* 9.8;
+ax = acc(1);
+ay = acc(2);
+az = acc(3);
 
+%conversione temperatura a gradi:
+T = TEMP./340 + 36.53;
+
+%conversione velocità angolare in deg/s:
+omega = GYRO./convg;
+omx = omega(1);
+omy = omega(2);
+omz = omega(3);
 end
 
+function [vett] = converti_vettore(bytes)
+    vett = [
+        comp2(unisci_bytes(bytes(1), bytes(2)));
+        comp2(unisci_bytes(bytes(3), bytes(4)));
+        comp2(unisci_bytes(bytes(5), bytes(6)))
+        ];
+end
+
+function [val] = unisci_bytes(H, L)
+    val = double(H) * 256 + double(L);
+end
+
+function [val] = comp2(val)
+    if val>2^15
+        val = val-2^16;
+    end 
+end
