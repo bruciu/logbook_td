40ec873c84f472431c94e11f3b5c024c45e67c6e
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-17 23:44:47 +0100

    Update es_5_veloce.m

--- settimana_13/scripts/es_5_veloce.m
+++ settimana_13/scripts/es_5_veloce.m
@@ -1,12 +1,23 @@
 path = "tmp/";
 
-A  = letturahex('tmp/Sere_1_velocissima.txt', 2000.*pi./180, 8);
+A  = letturahex('tmp/Sere_2_velocissima_6bw.txt', 2000.*pi./180, 8);
 
-ax = A(:,1);
-ay = A(:,2);
-az = A(:,3);
-wx = A(:,1);
-wy = A(:,1);
-wz = A(:,1);
 
-tempi = 1e-3 .* ((1:numel(ax))-1); 
+accelerazioni = [];
+accelerazioni(1, :) = A(:,1)';
+accelerazioni(2, :) = A(:,2)';
+accelerazioni(3, :) = A(:,3)';
+pulsazioni(1,:) = A(:,4);
+pulsazioni(2,:) = A(:,5);
+pulsazioni(3,:) = A(:,6);
+
+% ax = A(:,1);
+% ay = A(:,2);
+% az = A(:,3);
+% wx = A(:,1);
+% wy = A(:,1);
+% wz = A(:,1);
+
+tempo = 1e-3 .* ((1:numel(ax))-1); 
+
+save("dataSere_2_velocissima_6bw.mat", "accelerazioni", "pulsazioni", "tempo");
