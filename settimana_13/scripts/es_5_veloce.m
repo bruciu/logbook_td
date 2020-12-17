c6b4550c3105670cdea4bc4f94f21defe4f9e3ac
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2020-12-17 22:56:59 +0100

    dfghjk

--- settimana_13/scripts/es_5_veloce.m
+++ settimana_13/scripts/es_5_veloce.m
@@ -1,3 +1,12 @@
 path = "tmp/";
 
-A  = letturahex('tmp/Sere_1_velocissima.txt');
+A  = letturahex('tmp/Sere_1_velocissima.txt', 2000.*pi./180, 8);
+
+ax = A(:,1);
+ay = A(:,2);
+az = A(:,3);
+wx = A(:,1);
+wy = A(:,1);
+wz = A(:,1);
+
+tempi = 1e-3 .* ((1:numel(ax))-1); 
--- settimana_13/scripts/letturahex.m
+++ settimana_13/scripts/letturahex.m
@@ -1,4 +1,4 @@
-function [A] = letturahex(path)
+function [A] = letturahex(path, GFS, AFS)
 %LETTURAHEX Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -21,11 +21,17 @@ A = [];
 fid=fopen(path);
 while ~feof(fid)
     riga = fgetl(fid);
+    if numel(riga) < 35
     riga = hex2dec(regexp(riga,'\t','split'));
-    A = [A; riga'];
+        A = [A; riga'];
+    end
 end
 
-
+for ii =1:6
+    A(:, ii) = typecast(uint16(A(:, ii)),'int16');
+end
+A(:,1:3) = AFS*double(A(:,1:3))/2^15;
+A(:,4:6) = GFS*double(A(:,4:6))/2^15;
 %A = fscanf(filename, '%d') 
 
 %fclose(filename);
