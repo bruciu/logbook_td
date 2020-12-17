bdb9b02a065bb0fcb21b82d4547b5140b7231ed6
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-12-17 23:55:35 +0100

    Update es_5image.m

--- settimana_13/scripts/es_5image.m
+++ settimana_13/scripts/es_5image.m
@@ -10,32 +10,32 @@ tempo = tempi;
 
 %figura accx
 image(accelerazioni(1, :), tempo, "Accelerazione x", "m/s^2");
-+name = 'tmp/es_5_'+session+'_acc_x_.png';
+name = 'tmp/es_5_'+session+'_acc_x_.png';
 saveas(gcf,name);
 
 %figura accy
 image(accelerazioni(2, :), tempo, "Accelerazione y", "m/s^2");
-+name = 'tmp/es_5_'+session+'_acc_y_.png';
+name = 'tmp/es_5_'+session+'_acc_y_.png';
 saveas(gcf,name);
 
 %figura accz
 image(accelerazioni(3, :), tempo, "Accelerazione z", "m/s^2");
-+name = 'tmp/es_5_'+session+'_acc_z_.png';
+name = 'tmp/es_5_'+session+'_acc_z_.png';
 saveas(gcf,name);
 
 %figura wx
 image(pulsazioni(1, :), tempo, "Velocità angolare x", "rad/s");
-+name = 'tmp/es_5_'+session+'_w_x_.png';
+name = 'tmp/es_5_'+session+'_w_x_.png';
 saveas(gcf,name);
 
 %figura wy
 image(pulsazioni(2, :), tempo, "Velocità angolare y", "rad/s");
-+name = 'tmp/es_5_'+session+'_w_y_.png';
+name = 'tmp/es_5_'+session+'_w_y_.png';
 saveas(gcf,name);
 
 %figura wx
 image(pulsazioni(3, :), tempo, "Velocità angolare z", "rad/s");
-+name = 'tmp/es_5_'+session+'_w_z_.png';
+name = 'tmp/es_5_'+session+'_w_z_.png';
 saveas(gcf,name);
 
 %figura temperatura
