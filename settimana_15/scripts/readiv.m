879f545a5dcaa2864d6bbbb18e21256b71635717
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2021-02-24 22:21:37 +0100

    Update readiv.m

--- settimana_15/scripts/readiv.m
+++ settimana_15/scripts/readiv.m
@@ -1,12 +1,19 @@
 function [T] = readiv(path)
+
 [fid, mess] = fopen(path, 'r');
-if rid<0
+
+if fid<0
     error("no entry")
 end
 
 formatSpec = '%s %s %s %s %f %f';
 sizeA = [6 1];
-A = fscanf(fileID,formatSpec,sizeA)
+A = fscanf(fid,formatSpec,sizeA)
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fid,formatSpec,sizeA)
+
+T = 0;
 
 end
 
