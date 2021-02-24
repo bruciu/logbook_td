a4fda857e01cff5869ae9ed05635092dabb78132
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2021-02-24 22:29:59 +0100

    Update readiv.m

--- settimana_15/scripts/readiv.m
+++ settimana_15/scripts/readiv.m
@@ -1,19 +1,24 @@
-function [T] = readiv(path)
+function [T, Tensioni, Correnti] = readiv(path)
 
 [fid, mess] = fopen(path, 'r');
 
 if fid<0
-    error("no entry")
+    error("no entry");
 end
 
-formatSpec = '%s %s %s %s %f %f';
-sizeA = [6 1];
-A = fscanf(fid,formatSpec,sizeA)
+cs = fgets(fid);
+T = sscanf(cs(20:end), '%f %f');
+
+%formatSpec = '%s %s %s %s %f %f';
+%sizeA = [6 1];
+%A = fscanf(fid,formatSpec,sizeA)
+
 formatSpec = '%f %f';
 sizeA = [2 Inf];
-A = fscanf(fid,formatSpec,sizeA)
+A = fscanf(fid,formatSpec,sizeA);
 
-T = 0;
+Tensioni = (A(1, :))'; % in mA
+Correnti = (A(2, :))'; % in V
 
 end
 
