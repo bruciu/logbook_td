39a777c74addd64b41b915787638443035213c46
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2021-02-27 18:51:22 +0100

    Update stimaEgap_es7_2_secondaversione.m

--- settimana_15/scripts/stimaEgap_es7_2_secondaversione.m
+++ settimana_15/scripts/stimaEgap_es7_2_secondaversione.m
@@ -6,34 +6,35 @@ ITm = Tm;
 dITm = dTm./ITm.^2;
 
 
-f = @(A, B, x) A - B.*x; 
-derf = @(A, B, x) - B;
+f = @(A, B, C, x) A - B.*x + C.*log10(x); 
+derf = @(A, B, C, x) - B + C./x;
 
-X0 = [1, 1];
+X0 = [1, 1, 0];
 
 fitfun = fittype(f);
 
 w = 1./dp;
 
-x = ITm; dx = dITm; y = log10(p); dy = dp./p;
+x = ITm; dx = dITm; y = log10(p.*10.^6); dy = dp./p; %la corrente l'ho messa in Ampere
 
     for j = 1:3
         [fitted_curve,gof] = fit(x,y,fitfun,'StartPoint',X0, 'Weight', w);
         params = coeffvalues(fitted_curve);
-        w = 1./sqrt((derf(params(1), params(2), x).* dx).^2 + (dy).^2);
+        w = 1./sqrt((derf(params(1), params(2), params(3), x).* dx).^2 + (dy).^2);
     end
-
+    
+params
 dparams = confint(fitted_curve);
-dparams = (dparams(2, :) - dparams(1, :)) ./ 2;
+dparams = (dparams(2, :) - dparams(1, :)) ./ 2
 
-residui = f(params(1), params(2), x) - y;
+residui = f(params(1), params(2), params(3), x) - y;
 CHI2 =  sum((residui.*w).^2);
 CHI2rid = CHI2/(numel(residui)-2);
 
 hold on;
 
 errorbar(x, y, dy./2, dy./2, dx/2, dx/2, 'k.-');
-plot(x, f(params(1), params(2), x), '-');
+plot(x, f(params(1), params(2), params(3), x), '-');
 %errorbar(Tmk, p, dp./2, dp./2, dTm/2, dTm/2, 'k.-');
 %plot(ITm, f(params(1), params(2), ITm), '-');
 %fplot(@(Tmk) f(params(1), params(2), ITm));
