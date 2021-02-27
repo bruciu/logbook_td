b4dfc57d4ab6a442c0d89efd9c76732023bc14d6
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2021-02-27 23:28:04 +0100

    Update n2_es9_raccoltadatiutili.m

--- settimana_15/scripts/n2_es9_raccoltadatiutili.m
+++ settimana_15/scripts/n2_es9_raccoltadatiutili.m
@@ -32,35 +32,32 @@ for ii = 1:numel(runs)
     else
         if (j_min ~= 1)&&(j_min ~= numel(I_tmp))
             I_tmpa = [I_tmp(j_min-1), I_tmp(j_min)];
+            V_tmpa = [V_tmp(j_min-1), V_tmp(j_min)];
             flag = 2;
         else
             I_tmpa = I_tmp(j_min);
+            V_tmpa = V_tmp(j_min);
             flag = 1;
         end
     end
     
-    f = @(A, B, x) A + x.*B;
+    %f = @(A, B, x) A + x.*B;
+    If = @(A, B, x) - A./B + x./B;
     
     if flag == 2
-        B = (I_tmpa(2) - I_tmpa(1))/V_tmpa(2) - V_tmpa(1);
-    else 
-        
+        B = (I_tmpa(2) - I_tmpa(1))/(V_tmpa(2) - V_tmpa(1));
+        A = I_tmpa(2) - V_tmpa(2).*B;
+        valuey = mean_i;
+        valuex = If(A, B, mean_i);
+    else
+        valuey = mean_i;
+        valuex = V_tmpa;
     end
-    
-    
-    
-    for jj = 1:numel(I_tmp)
-        if abs(I_tmp(jj) - mean_i) <= semi_s
-            I = [I; I_tmp(jj)];
-            V = [V; V_tmp(jj)];
-            T = [T; T_tmp];
-            dT = [dT; dT_tmp];
-        end
-    end
-    
-    
+    I = [I; valuey];
+    V = [V; valuex];
+    T = [T; T_tmp];
+    dT = [dT; dT_tmp];
 end
-
 
 
 
