f3ee5b2e20e89e58a962043ef3d7ad08dd9377d0
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2021-03-01 22:27:00 +0100

    Create fit_folder_alta.m

---
+++ settimana_15/scripts/fit_folder_alta.m
@@ -0,0 +1,76 @@
+function [T, P, dP, chi2, chi2rid, fNames] = fit_folder_alta(folder, bool_plot)
+
+if nargin < 3
+    bool_plot = false;
+end
+
+T = [];
+P = [];
+dP = [];
+chi2 = [];
+chi2rid = [];
+fNames = [];
+
+m_epsylon = sqrt(sqrt(eps));% doppia radice per avere numeri più grandi
+epsylon = @(x) abs(x) .* m_epsylon + m_epsylon;
+
+% funzione di fit:
+f = @(Is, nVt, R, G, x) curr_G(x, Is, nVt, R, G);
+%derivata:
+derf = @(Is, nVt, R, G, x)...
+    (f(Is, nVt, R, G, x + epsylon(x)) - f(Is, nVt, R, G, x))./...
+    epsylon(x);
+
+
+fitfun = fittype(f);
+X0 = [0.6e-3, 0.045, 1.95, 0.9];
+runs = read_folder(folder);
+
+
+for i = 1:numel(runs)
+    run = runs(i);
+    
+    V = run.V;
+    I = run.I;
+    T = [T; run.T'];
+    fNames = [fNames; string(run.name)];
+    
+    dV = V * 0 + 0.1e-3;
+    dI = I * 0 + 0.4;
+    
+    w = 1 + V * 0;
+    
+    for j = 1:3
+        [fitted_curve] = fit(V, I,fitfun,'StartPoint',X0, 'Weight', w, 'Lower',[-Inf, -Inf, sqrt(eps)*2, 0.0], 'Upper', [Inf, Inf, Inf, 0.0]);
+        params = coeffvalues(fitted_curve);
+        w = 1./sqrt((derf(params(1), params(2), params(3), params(4), V).* dV(1)).^2 + (dI(1)).^2);
+    end
+    
+    dparams = confint(fitted_curve);
+    dparams = (dparams(2, :) - dparams(1, :)) ./ 2;
+    
+    P = [P; params];
+    dP = [dP; dparams];
+    
+    tmp = f(params(1), params(2), params(3), params(4), x);
+    
+    CHI2 =  sum(((tmp - I).*w).^2);
+    CHI2rid = CHI2/(numel(V)-3);
+    
+    chi2 = [chi2; CHI2];
+    chi2rid = [chi2rid; CHI2rid];
+    
+    if (bool_plot)
+        fig = figure;
+        hold on
+        plot(V, I, '.');
+        plot(V, tmp);
+        set(gca, 'YScale', 'log');
+        pause(1);
+        close(fig);
+    end
+end
+
+
+end
+
